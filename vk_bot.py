86e46c726dfa0499eebd400bdc8b83e753803432
Valery Efremov <valeriy131100@gmail.com>
2022-04-01 11:41:38 +0300

    Send buttons in vk bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,15 +1,26 @@
+import json
+import re
+
 import vk_api as vk
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 
 import config
 
 
-def echo(event, vk_api):
+def start(event, vk_api):
+    keyboard = VkKeyboard()
+    keyboard.add_button('Новый вопрос')
+    keyboard.add_button('Сдаться')
+    keyboard.add_line()
+    keyboard.add_button('Мой счёт')
+
     vk_api.messages.send(
         user_id=event.message.from_id,
-        message=event.message.text,
-        random_id=get_random_id()
+        message='Привет! Я бот для викторин!',
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard()
     )
 
 
@@ -18,11 +29,23 @@ if __name__ == "__main__":
     vk_api = vk_session.get_api()
     longpoll = VkBotLongPoll(vk_session, group_id=config.vk_group_id)
 
+    commands = {
+        r'^Начать$': start
+    }
+
     while True:
         try:
             for event in longpoll.listen():
-                if (event.type == VkBotEventType.MESSAGE_NEW
+                if not (event.type == VkBotEventType.MESSAGE_NEW
                         and event.from_user):
-                    echo(event, vk_api)
+                    continue
+
+                for command in commands:
+                    if not re.match(command, event.message.text):
+                        continue
+
+                    commands[command](event, vk_api)
+                    break
+
         except Exception as error:
             continue
