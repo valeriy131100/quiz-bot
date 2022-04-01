72532cc24fe369426c30f5e412567aa589dfe775
Valery Efremov <valeriy131100@gmail.com>
2022-04-01 12:06:15 +0300

    Add context and blank buttons functions

--- vk_bot.py
+++ vk_bot.py
@@ -1,22 +1,30 @@
-import json
-import re
+from dataclasses import dataclass, field
 
 import vk_api as vk
+from redis import Redis
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.keyboard import VkKeyboard
 from vk_api.utils import get_random_id
+from vk_api.vk_api import VkApiMethod
 
 import config
 
 
-def start(event, vk_api):
+@dataclass
+class Context:
+    vk_api: VkApiMethod
+    redis: Redis
+    current_questions: dict = field(default_factory=dict)
+
+
+def start(event, context):
     keyboard = VkKeyboard()
     keyboard.add_button('Новый вопрос')
     keyboard.add_button('Сдаться')
     keyboard.add_line()
     keyboard.add_button('Мой счёт')
 
-    vk_api.messages.send(
+    context.vk_api.messages.send(
         user_id=event.message.from_id,
         message='Привет! Я бот для викторин!',
         random_id=get_random_id(),
@@ -24,14 +32,30 @@ def start(event, vk_api):
     )
 
 
+def handle_new_question(event, context):
+    pass
+
+
+def handle_surrender(event, context):
+    pass
+
+
+def handle_answer(event, context):
+    pass
+
+
 if __name__ == "__main__":
     vk_session = vk.VkApi(token=config.vk_token, api_version='5.131')
-    vk_api = vk_session.get_api()
     longpoll = VkBotLongPoll(vk_session, group_id=config.vk_group_id)
 
-    commands = {
-        r'^Начать$': start
-    }
+    bot_context = Context(
+        vk_api=vk_session.get_api(),
+        redis=Redis(
+            host=config.redis_host,
+            port=config.redis_port,
+            password=config.redis_password
+        )
+    )
 
     while True:
         try:
@@ -40,12 +64,16 @@ if __name__ == "__main__":
                         and event.from_user):
                     continue
 
-                for command in commands:
-                    if not re.match(command, event.message.text):
-                        continue
+                command = event.message.text
 
-                    commands[command](event, vk_api)
-                    break
+                if command == 'Начать':
+                    start(event, bot_context)
+                elif command == 'Новый вопрос':
+                    handle_new_question(event, bot_context)
+                elif command == 'Сдаться':
+                    handle_surrender(event, bot_context)
+                else:
+                    handle_answer(event, bot_context)
 
         except Exception as error:
             continue
